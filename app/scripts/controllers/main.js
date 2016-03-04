fdf9469ab12be6f5e805abe27529b99480f74533
Jaakko Kemppainen <jaakko.kemppainen@student.tut.fi>
2016-03-04 17:50:02 +0200

    Changed the animation to use CSS3 instead

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('jpApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
     ];
   });
